7eaf895621cffc9b3f30438c06d87dd00b907652
mureT-ani <72783124+mureT-ani@users.noreply.github.com>
2021-03-17 21:41:30 +0900

    posts テーブルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_03_17_072940) do
   end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
