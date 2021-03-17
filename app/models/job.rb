668a13b66300495c20ffef6d3165742fecec9b08
mureT-ani <72783124+mureT-ani@users.noreply.github.com>
2021-03-17 14:00:08 +0900

    ユーザー認証機能の実装

---
+++ app/models/job.rb
@@ -0,0 +1,11 @@
+class Job < ActiveHash::Base
+  self.data = [
+    {id: 1, name: '--'},
+    {id: 2, name: '学生'},
+    {id: 3, name: '研究職'},
+    {id: 4, name: 'その他'}
+  ]
+
+  include ActiveHash::Associations
+  has_many :users
+end
