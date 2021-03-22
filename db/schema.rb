0dece2844591789fcbf912cb42b827b1d92df9f0
mureT-ani <72783124+mureT-ani@users.noreply.github.com>
2021-03-22 15:50:11 +0900

    Goodモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_072940) do
+ActiveRecord::Schema.define(version: 2021_03_22_064723) do
 
   create_table "action_text_rich_texts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,15 @@ ActiveRecord::Schema.define(version: 2021_03_17_072940) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "goods", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_goods_on_post_id"
+    t.index ["user_id"], name: "index_goods_on_user_id"
+  end
+
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
     t.bigint "user_id", null: false
@@ -67,5 +76,7 @@ ActiveRecord::Schema.define(version: 2021_03_17_072940) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "goods", "posts"
+  add_foreign_key "goods", "users"
   add_foreign_key "posts", "users"
 end
