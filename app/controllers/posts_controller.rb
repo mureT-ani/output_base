eaeb64f4aef1e4ce25200574675168a10be667c0
mureT-ani <72783124+mureT-ani@users.noreply.github.com>
2021-04-26 13:29:45 +0900

    タグ表示機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :contributor_check, only: %i[edit update destroy]
 
   def index
-    @post = Post.includes(:user).order('created_at DESC')
+    @post = Post.includes(:user, :tags).order('created_at DESC')
   end
 
   def new
